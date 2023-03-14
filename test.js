9ea99ded454afd1a1d5eefb3a1ff75201f61271d
ahmed <ahmed.maw3d@gmail.com>
2023-03-14 23:44:53 +0200

    add test.js

---
+++ test.js
@@ -0,0 +1,22 @@
+var person ={firstName:"Ahmed",lastName:"mawed",gender:"m",age:"28"};
+
+document.getElementById("test").innerHTML=
+    
+    "my firstName Is: " + person.firstName + "<br>" +
+    
+    "my LastName Is: " +person.lastName+ "<br>" +
+    
+    "gender Is: " + person.gender+ "<br>" +
+    
+    "Age Is: " + person.age;
+
+/*var x = 100;
+
+document.getElementById("test").innerHTML +="100 <br>";
+
+document.getElementById("test").innerHTML +="100<br>";
+
+document.getElementById("test").innerHTML +="100<br>";
+
+document.getElementById("test").innerHTML +="100";*/
+
