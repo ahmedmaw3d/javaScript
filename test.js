a7f3f33878fc8996c42323804919eac2d8be9acc
ahmed <ahmed.maw3d@gmail.com>
2023-03-15 15:31:41 +0200

    test.js

--- test.js
+++ test.js
@@ -1,4 +1,87 @@
-var person ={firstName:"Ahmed",lastName:"mawed",gender:"m",age:"28"};
+/*function calcNumber(a , b , c){
+    "ues strict";
+
+    return a + b + c;
+  
+}
+document.getElementById("test").innerHTML = calcNumber(10,350,20);*/
+/*function calcDays(age){
+  "ues strict";
+
+  var yeas = 365;
+
+  return yeas * age;
+}
+var dynamicAge = calcDays (prompt("whats your Age ?"));
+
+document.getElementById("test").innerHTML = dynamicAge;*/
+/*function sayHello(firstName,middleName,lastName){
+    "use strict";
+
+    return "Hello " + firstName + " " + middleName + " "+ lastName + " How are you today ";
+}
+
+document.getElementById("test").innerHTML = sayHello("Malik","Ahmed","Mawed");*/
+/*function myAgeInhours(){
+
+  var myAge = 28;
+
+  return myAge * 365 * 24;
+}
+var hoursCalc = myAgeInhours();
+
+document.getElementById("test").innerHTML = "your Age In day = " + hoursCalc +"days";*/
+/*function myInfo(){
+
+  var myName = "Ahmed",
+
+      myAge = 28;
+
+      return myName;
+}
+var myFunction = myInfo();
+
+document.getElementById("test").innerHTML = myFunction;*/
+/*function seyHi(){
+  
+    "use Strict";
+    var myName = "Ahmed";
+
+    if(myName==="Ahmed"){
+
+        alert("Hello " + myName +" you are wolcome ");
+    }else{
+
+      alert("Hello " + myName +" you are not wolcome ");
+    }
+}*/
+/*var Name = "Ahmed",
+
+    age = 28,
+
+    address = "KAN";
+
+
+if(Name === "ahmed" || age === 28 || address === "KAN"){
+
+  alert("Good");
+
+}else{
+
+  alert("Bad");
+
+}*/
+/*var myPrice = 50;
+
+if(myPrice == 60)
+ {
+   alert("Good");
+ }
+else
+ {
+   alert("Bad");
+ }*/
+/*var person ={firstName:"Ahmed",lastName:"mawed",gender:"m",age:"28"};
 
 document.getElementById("test").innerHTML=
     
@@ -8,7 +91,7 @@ document.getElementById("test").innerHTML=
     
     "gender Is: " + person.gender+ "<br>" +
     
-    "Age Is: " + person.age;
+    "Age Is: " + person.age;*/
 
 /*var x = 100;
 
